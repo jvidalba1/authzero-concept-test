871a1adaa083aa2eb58ca0ffedac05002f23df29
Mateo Vidal <jvidalba@gmail.com>
2014-12-16 12:00:35 -0500

    pages controller and configuration to log in with auth0

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 AuthzeroConceptTest::Application.routes.draw do
-  get "auth0/callback"
-  get "auth0/failure"
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => "auth0#failure"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
