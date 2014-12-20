9a425aeacf952c9c47dea6951c78dfc979aabed8
Mateo Vidal <jvidalba@gmail.com>
2014-12-20 01:54:01 -0500

    creating an user local and remote auth0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
+  include HTTParty
+  format :json
+  headers 'Accept' => 'application/json'
   # respond_to :json
+
+  def post(path, options={})
+    self.class.post(URI.encode(Figaro.env.endpoint + path), options)
+  end
 
   def show
     # respond_with User.find(params[:id])
@@ -14,8 +21,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # here is where I have to make the request
+      create_auth0_user(@user)
       redirect_to root_path
-
     else
       render 'new'
     end
@@ -25,6 +32,36 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def create_auth0_user(user, options={})
+    access_token = get_access_token["access_token"]
+    body = {  email: user.email,
+              password: user.password,
+              connection: "Username-Password-Authentication"#,
+              # email_verified: false
+            }.merge(options)
+    post("/api/users?access_token=#{access_token}", body: body)
+  end
+
+  def get_access_token
+    post("/oauth/token", body: {
+                                  client_id:      Figaro.env.client_id,
+                                  client_secret:  Figaro.env.client_secret,
+                                  grant_type:     "client_credentials"
+                                }).merge({ timestamp: Time.now })
+  end
+
+  def authenticate
+    if @access_token
+      if @access_token[:timestamp] < Time.now - 60*60*20
+        @access_token = get_access_token
+      else
+        @access_token
+      end
+    else
+      @access_token = get_access_token
+    end
   end
 
   private
